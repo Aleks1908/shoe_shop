0c2aa4976d504f30bde533c98768484557e6d51c
Isak-Bego <65165135+Isak-Bego@users.noreply.github.com>
2024-11-23 15:37:01 +0200

    #19 Create a Database Layer for the Backend (#32)
    * Create template readme for the express app
    
    * Connect to the databbase
    
    * Minor tweaks

---
+++ services/express-api/src/database/db-config.js
@@ -0,0 +1,22 @@
+import { MongoClient } from "mongodb";
+import {logger} from "../server/config/logger-config.js";
+
+const connectionString = process.env.ATLAS_URI || "";
+const client = new MongoClient(connectionString);
+let conn;
+let db; 
+
+try {
+  logger.debug("Trying to connect...");
+  conn = await client.connect();
+  logger.debug("Connection successful.");
+  db = conn.db("shoe-shop");
+  logger.debug("Pinging Database..."); 
+  await db.command({ping: 1});
+  logger.debug("Pong.");
+} catch(e) {
+  logger.error(e.toString());
+}
+
+
+export default db;
