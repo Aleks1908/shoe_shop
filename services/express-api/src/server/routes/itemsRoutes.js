d4d38dd04cfa9ac4ba6fced76422d281d691c5e8
Isak-Bego <65165135+Isak-Bego@users.noreply.github.com>
2024-11-23 19:05:15 +0200

    #18 implement swagger docs (#36)
    * Add swagger support

--- services/express-api/src/server/routes/itemsRoutes.js
+++ services/express-api/src/server/routes/itemsRoutes.js
@@ -30,3 +30,83 @@ itemRouter.get('/limited', (req, res) => {
 
 
 export default itemRouter; 
+
+/**
+ * @swagger
+ * tags:
+ *   name: /items
+ *   description: API endpoints for managing products
+ */
+
+/**
+ * @swagger
+ * /shoes:
+ *   get:
+ *     summary: Returns a JSON containing all shoes
+ *     tags: 
+ *       - /items
+ *     responses:
+ *       200:   
+ *         description: A successful response
+ * 
+ */
+ 
+ /** 
+ * @swagger
+ * /clothes:
+ *   get:
+ *     summary: Returns a JSON containing all clothes
+ *     tags: 
+ *       - /items  
+ *     responses:
+ *       200:   
+ *         description: A successful response
+ */
+
+ /**
+ * @swagger
+ * /accessories:
+ *   get:
+ *     summary: Returns a JSON containing all accessories
+ *     tags: 
+ *       - /items
+ *     responses:
+ *       200:   
+ *         description: A successful response
+ */
+
+ /**
+ * @swagger
+ * /hats:
+ *   get:
+ *     summary: Returns a JSON containing all hats
+ *     tags: 
+ *       - /items
+ *     responses:
+ *       200:   
+ *         description: A successful response
+ */
+
+ /**
+ * @swagger
+ * /slippers:
+ *   get:
+ *     summary: Returns a JSON containing all slippers
+ *     tags: 
+ *       - /items
+ *     responses:
+ *       200:   
+ *         description: A successful response
+ */
+
+ /**
+ * @swagger
+ * /limited:
+ *   get:
+ *     summary: Returns a JSON containing all limited items
+ *     tags: 
+ *       - /items
+ *     responses:
+ *       200:   
+ *         description: A successful response
+ */
