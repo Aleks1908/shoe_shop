e6b21c251e476d3eb70589350b73b3c0c55c0e98
Isak Bego <isakbego@gmail.com>
2024-11-23 16:27:50 +0200

    Create a routing system

---
+++ services/express-api/src/server/routes/favoritesRoutes.js
@@ -0,0 +1,27 @@
+import { Router } from "express";
+import { logger } from "../config/logger-config.js"
+
+const favoritesRouter = Router();
+
+//should only be accessible by the authenticated users so we should implement a middleware for this route
+//TO-DO: Update is so that it uses an authetication service
+favoritesRouter.use((req, res, next) => {
+    logger.debug("Passing through the middleware");
+    next();
+})
+
+favoritesRouter.get("/", (req, res) => {
+    res.send('Route not implemented');
+});
+
+favoritesRouter.post("/", (req, res) => {
+    res.send('Route not implemented');
+});
+
+favoritesRouter.delete("/",(req, res) => {
+    res.send('Route not implemented');
+});
+
+
+
+export default favoritesRouter; 
