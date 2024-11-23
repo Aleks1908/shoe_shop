cffcc2ca603b1b102b4b12a5930e9ca3bb962d25
Aleks Ivanov <al.ivanov1908@gmail.com>
2024-11-23 13:59:57 +0200

    #6 Footer section (#25)
    * create footer section
    
    * rework struct
    
    ---------
    
    Co-authored-by: Aleks4o <aleks4o@Aleks4os-MacBook-Pro.local>

---
+++ services/web/src/Components/FooterSection/FooterSection.jsx
@@ -0,0 +1,43 @@
+import { AiFillFacebook } from 'react-icons/ai';
+import { AiFillInstagram } from 'react-icons/ai';
+import { AiOutlineTwitter } from 'react-icons/ai';
+import './footer_section.css';
+
+export const FooterSection = () => {
+    return (
+        <div className='footer_section'>
+            <div className='social_media'>
+                <div className='social_media_icon'>
+                    <a href='#facebook'>
+                        <AiFillFacebook/>
+                    </a>
+                </div>
+                <div className='social_media_icon'>
+                    <a href="#instagram">
+                        <AiFillInstagram/>
+                    </a>
+                </div>
+                <div className='social_media_icon'>
+                    <a href='#twitter'>
+                        <AiOutlineTwitter/>
+                    </a>
+                </div>
+            </div>
+            <div className='info'>
+                <a href='#info'>Info</a>
+                <span>.</span>
+                <a href='#support'>Support</a>
+                <span>.</span>
+                <a href='#marketing'>Marketing</a>
+            </div>
+            <div className='info'>
+                <a href='#tou'>Terms of Use</a>
+                <span>.</span>
+                <a href='#pp'>Privacy Policy</a>
+            </div>
+            <div className='creator_info'>
+                <p>© Alekandar Ivanov</p>
+            </div>
+        </div>
+    )
+}
