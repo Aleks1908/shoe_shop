158dc87ec59ff2799e64c8c2db0062ac0c9e9ea3
Aleks Ivanov <al.ivanov1908@gmail.com>
2024-11-23 14:35:27 +0200

    #9 Product component (#30)
    * added product component
    
    * rework struct
    
    * rework product component struct
    
    * rework product section
    
    ---------
    
    Co-authored-by: Aleks4o <aleks4o@Aleks4os-MacBook-Pro.local>

--- services/web/src/main.jsx
+++ services/web/src/main.jsx
@@ -1,6 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
+import { Content } from "./Components/Content";
+
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode></React.StrictMode>
+  <React.StrictMode>
+    <Content />
+  </React.StrictMode>
 );
