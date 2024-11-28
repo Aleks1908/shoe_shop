d64e80305f90b5cb1ecb011579f246b5c6fac9da
Isak Bego <isakbego@gmail.com>
2024-11-28 14:54:06 +0200

    Add swagger docs for the database models

--- services/express-api/src/database/models/usersModel.js
+++ services/express-api/src/database/models/usersModel.js
@@ -11,3 +11,37 @@ userSchema.index({user_name: 1}, {unique: true});
 const userModel = new model("User", userSchema);
 
 export default userModel;
+
+
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     User:
+ *       type: object
+ *       required:
+ *         - user_name
+ *         - password
+ *       properties:
+ *         user_name:
+ *           type: string
+ *           description: The unique username of the user.
+ *         password:
+ *           type: string
+ *           description: The user's password.
+ *         favorites:
+ *           type: array
+ *           items:
+ *             type: string
+ *           description: List of the user's favorite items.
+ *       example:
+ *         user_name: "testUser"
+ *         password: "testPassword"
+ *         favorites: ["item1", "item2"]
+ * 
+ *     UniqueIndexes:
+ *       properties:
+ *         user_name: 
+ *           description: "The `user_name` field is unique for each user."
+ *           type: "string"
+ */
